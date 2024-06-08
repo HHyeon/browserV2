5bb797f42652e13e0e0b72e529bb452c558e854f
rpi5wevdev <sunki1123@naver.com>
2024-06-09 01:45:43 +0900

    ENHANCED

---
+++ dirseek.php
@@ -0,0 +1,57 @@
+<?php
+
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+
+
+
+$jsonresult = array();
+$jsonresult["ret"]=FALSE;
+$data = array();
+
+if(isset($_GET["x"]))
+{
+    $path=$_GET["x"];
+
+    if(substr($path, -1) == '/')
+        $path = rtrim($path, '/');
+
+    // console_log($path);
+
+    $dirhandle=opendir($path);
+    
+    if($dirhandle)
+    {
+        while(($entry=readdir($dirhandle)) !== FALSE)
+        {
+            if(substr($entry, 0, 1) == ".") continue;
+
+            $timedata = array();
+            $timedata["t"] = date('Y-m-d H:i:s',filemtime($path.'/'.$entry));
+            $timedata["d"] = $entry;
+
+            array_push($data, $timedata);
+        }
+        
+        $jsonresult["ret"]=TRUE;
+        closedir($dirhandle);
+    }
+
+
+
+}
+else
+{
+    // console_log("input params ".$input." not given");
+}
+
+// foreach($data as $each) console_log($each);
+
+$jsonresult["data"] = $data;
+echo json_encode($jsonresult);
+?>
--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 
+const urlParams = (new URL(window.location.href)).searchParams;
+let parampath = urlParams.get('p');
+let parampathgiven = false;
 
+if(parampath != null || parampath != undefined)
+    if(parampath[parampath.length-1] == '/') parampath = parampath.substring(0, parampath.length-1);
+else
+    parampathgiven = true;
+
+if(parampath == null) parampath = '.';
+// console.log(`parampath - ${parampath}`);
+
+let dirlist = [];
 let queryedlist;
 let item_w, item_h;
 let TopScrollView = document.getElementById('scroll-views');
+let MainTitle = document.getElementById('MainTitle');
 let visual_pictures_row = 4;
 let visual_pictures_col = 4;
 
 function runquery() {
     let xmlhttp = new XMLHttpRequest();
     let url=`query.php`;
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send(null);
+    return xmlhttp.responseText;
+};
+
+function dirseek(param) {
+    let xmlhttp = new XMLHttpRequest();
+    let url=`dirseek.php?x=${param}`;
     xmlhttp.open("GET", url, false);
     xmlhttp.send(null);
     return xmlhttp.responseText;
@@ -23,26 +44,12 @@ function refreshinginfinitylist()
         const pos_x = item_w * (i%visual_pictures_row);
         const pos_y = item_h * (Math.floor(i/visual_pictures_row)+scrolleditemidx);
 
-        if(queryedlist.length <= i+(scrolleditemidx*visual_pictures_row)) break;
+        let pos = i+(scrolleditemidx*visual_pictures_row);
 
-        let imgpath = queryedlist[i+(scrolleditemidx*visual_pictures_row)];
-        let imgtext = imgpath;
-
-        try
-        {
-            let p1 = imgtext.indexOf('/');
-            if(p1 >= 0)
-            {
-                imgtext = imgtext.substring(p1+1, imgtext.length);
-                p1 = imgtext.indexOf('/');
-                imgtext = imgtext = imgtext.substring(0, p1);
-            }
-        }
-        catch(ex) {
-            console.log(ex);
-        }
+        if(dirlist.length <= pos) break;
+        let dirlist_item = dirlist[pos];
         
-        TopScrollView.innerHTML += makeitem(item_w, item_h, pos_x, pos_y, imgpath, imgtext);
+        TopScrollView.innerHTML += makeitem(item_w, item_h, pos_x, pos_y, dirlist_item.fname, dirlist_item.text);
     }
 }
 
@@ -72,37 +79,112 @@ window.addEventListener("scroll", function(e) {
 
 });
 
-function makeitem(w,h,x,y,imgpath,text) {
-    return `<div class="item" style="width: ${w}px; height: ${h}px; transform: translate(${x}px, ${y}px); position: absolute;">
-                <div style="box-sizing: border-box; overflow: hidden; position: absolute; width: 100%; height: 100%;" >
+function randChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function makeitem(w,h,x,y,fname,text) {
+    let ret;
+    let imgpath = '';
+    
+    let belowdirseekpath = `${parampath}/${fname}`;
+    console.log(`belowdirseekpath - ${belowdirseekpath}`);
+
+    const jsondata = JSON.parse(dirseek(belowdirseekpath));
+
+    if(jsondata["ret"]) // openable directory
+    {
+        let dirbelowimgs = [];
+
+        jsondata["data"].forEach(each => {
+            const name = each["d"];
+            const time = each["t"];
+            
+            let fnameext = name.substring(name.lastIndexOf('.')+1);
+            if(fnameext == 'jpeg' || fnameext == 'jpg') dirbelowimgs.push(name);
+        })
+
+        if(dirbelowimgs.length == 0)
+            imgpath = '';
+        else
+            imgpath = belowdirseekpath + "/" + randChoice(dirbelowimgs);
+
+        ret = `<div class="item" style="width: ${w}px; height: ${h}px; transform: translate(${x}px, ${y}px); position: absolute;">
+                <div style="box-sizing: border-box; overflow: hidden; position: absolute; width: 100%; height: 100%;" >` +
+
+                (parampathgiven ? 
+                `<a href="${document.location.href}/${fname}"></a>` :
+                `<a href="${document.location.href}?p=${fname}"></a>` ) +
+
+                    `<div class="layer-text" style="box-sizing:border-box">
+                        <h3>${text}</h3>
+                    </div>
+                    <img src="${imgpath}" alt="Cover" style="position: absolute; width: 100%; height: 100%; object-fit:cover; "}}>
+                </div>
+            </div>`
+    }
+    else // just A File
+    {
+        let imgvisible = false;
+    
+        let fnameext = fname.substring(fname.lastIndexOf('.')+1);
+        if(fnameext == 'jpeg' || fnameext == 'jpg') imgvisible = true;
+        if(imgvisible) imgpath = parampath + "/" + fname;
+
+        ret = `<div class="item" style="width: ${w}px; height: ${h}px; transform: translate(${x}px, ${y}px); position: absolute;">
+                <div style="box-sizing: border-box; overflow: hidden; position: absolute; width: 100%; height: 100%;" >\
                     <div class="layer-text" style="box-sizing:border-box">
                         <h3>${text}</h3>
                     </div>
-                    <img src="${imgpath}" alt="Cover" style="position: absolute; width: 100%; height: 100%;">
+                    <img src="${imgpath}" alt="Cover" style="position: absolute; width: 100%; height: 100%; object-fit:cover; "}}>
                 </div>
             </div>`
+    }
+
+    return ret;
 }
 
 function startup() {
-    let MainTitle = document.getElementById('MainTitle');
-
-    TopScrollView.innerHTML = '';
-
-    const jsondata=JSON.parse(runquery());
-    
     item_w = TopScrollView.clientWidth/visual_pictures_row;
     item_h = item_w/3*4; // height/width be 4:3 ratio
-    
+
+    const jsondata=JSON.parse(dirseek(parampath)); // runquery()
     if(jsondata["ret"])
     {
         queryedlist = jsondata["data"];
-        
+
+        queryedlist.forEach(each => {
+
+            const name = each["d"];
+            const time = each["t"];
+            
+            let imgtext;
+            try
+            {
+                imgtext = name;
+                let p1 = imgtext.indexOf('/');
+                if(p1 >= 0)
+                {
+                    imgtext = imgtext.substring(p1+1, imgtext.length);
+                    p1 = imgtext.indexOf('/');
+                    imgtext = imgtext = imgtext.substring(0, p1);
+                }
+            }
+            catch(ex) {
+                imgtext = '';
+                console.log(ex);
+            }
+
+            dirlist.push(
+                {
+                    fname: name,
+                    text: imgtext,
+                }
+            );
+        });
+
         refreshinginfinitylist();
-    }
-    else
-    {
-        MainTitle.style.display = 'block';
-        MainTitle.innerText = 'Querying Error !';
+        MainTitle.style.display = 'none';
     }
 }
 
